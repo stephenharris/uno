60540d9f55fbf60f4bbc75ac734a5f0e427ff8ee
Stephen Harris <contact@stephenharris.info>
2020-05-17 21:19:57 +0100

    Fix compile error

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import classNames from '../../class-names';
 import './Button.css';
+import cn from 'classnames';
 
 function Button({children, onClick, large = false, small = false, disabled = false}) {
 
 
   return (
-    <button type="button" disabled={disabled} onClick={onClick} className={classNames({
+    <button type="button" disabled={disabled} onClick={onClick} className={cn({
       'button': true,
       'button--small': small,
       'button--large': large,
