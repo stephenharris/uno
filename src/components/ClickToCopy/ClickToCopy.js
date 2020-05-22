6524a834527841aca73310676aff81731921df77
Stephen Harris <contact@stephenharris.info>
2020-05-22 22:12:17 +0100

    Improve awaiting players screen and add click-to-copy

---
+++ src/components/ClickToCopy/ClickToCopy.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const ClickToCopy = ({textToCopy, children, onCopy}) => {
+
+    const ref = React.createRef();
+
+    const copyToClipboard = (event) => {
+       event.preventDefault();
+       ref.current.select();
+       document.execCommand('copy');
+       event.target.focus();
+       if(onCopy) {
+           onCopy();
+       }
+    }
+
+    return (
+    <div style={{"display":"inline"}} onClick={copyToClipboard} >
+        {children}
+        <textarea
+            style={{"width":"1px", "height":"1px", "background":"none", "border":"none",
+            "left": "-10px",
+            "overflow": "hidden",
+            "position": "absolute"}}
+            tabIndex="-1"
+            ref={ref}
+            defaultValue={textToCopy}
+        />
+    </div>
+  );
+};
+
+export default ClickToCopy;
--- src/screens/AwaitingPlayersScreen/AwaitingPlayersScreen.js
+++ src/screens/AwaitingPlayersScreen/AwaitingPlayersScreen.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import Button from '../../components/Button/Button'
 import './AwaitingPlayersScreen.css';
 import cn from 'classnames';
+import ClickToCopy from '../../components/ClickToCopy/ClickToCopy';
 
 function AwaitingPlayersScreen({gameState, onStartGame}) {
 
@@ -15,8 +16,23 @@ function AwaitingPlayersScreen({gameState, onStartGame}) {
   return (
     <div>
       <h2 className="h2">Awaiting players</h2>
+
+
       <div className="awaitingPlayerList">
-        <ul>
+        <h3>Share the link</h3>
+        <p>Other players can join using the link below</p>
+        <p class="shareUrl">
+          {`${process.env.REACT_APP_URL}/${gameState.id}`}
+          <ClickToCopy textToCopy={`${process.env.REACT_APP_URL}/${gameState.id}`}>
+            <svg className="clickToCopy" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <rect className="clickToCopy__primary" width="14" height="14" x="3" y="3" rx="2"></rect>
+              <rect className="clickToCopy__secondary" width="14" height="14" x="7" y="7" rx="2"></rect>
+            </svg>
+          </ClickToCopy>
+        </p>
+
+        <h3>Players in game</h3>
+        <ol>
           {gameState.players && gameState.players.map((player) => {
             return (<li 
               key={player.id}
@@ -26,8 +42,12 @@ function AwaitingPlayersScreen({gameState, onStartGame}) {
               })}
               >{player.name}</li>) 
           })}
-        </ul>
+        </ol>
 
+
+
+        <h3>Start the game</h3>
+        <p>Once everyone has joined, start the game</p>
         <Button disabled={loading} onClick={onClickStartGame}>Start game!</Button>
       </div>
     </div>
